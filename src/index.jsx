1f35a76b4c02cc26f62c7a206a1f75bc633f958b
SurendherReddy <surendher4423h@gmail.com>
2024-05-10 15:32:09 +0530

    assign

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import AppClass from "./AppClass";
+import './App.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1 id='title'>Kalvium Gallery</h1>
     <App />
     <AppClass />
   </React.StrictMode>,
