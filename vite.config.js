64747e7dcb5e6642c42abfa22cc4f2a1dd947cb4
SurendherReddy <surendher4423h@gmail.com>
2024-05-11 03:05:40 +0530

    com

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base:"/lab-react-jsx/",
     plugins: [react()],
-    
+    base:"/lab-react-jsx/",
   };
 });
